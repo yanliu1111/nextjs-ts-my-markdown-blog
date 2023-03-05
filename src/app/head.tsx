45fec530489e9ddf389aeb052f3d80a9673f0884
yanliu1111 <liuyan0504@gmail.com>
2023-03-05 15:51:56 -0700

    add: add og in head

--- src/app/head.tsx
+++ src/app/head.tsx
@@ -2,7 +2,23 @@ export default function Head() {
   return (
     <>
       <title>Yan's Code Blog</title>
-      <meta content="width= device-width, initial-scale=1" name="description" />
+      <meta name="description" content="yan's code blog" />
+      <meta name="author" content="Yan Liu" />
+      <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+
+      <meta property="og:title" content="Yans Code Blog" />
+      <meta property="og:type" content="website" />
+      <meta property="og:url" content="https://www.yancodeblog.codes/" />
+      <meta
+        property="og:image"
+        content="https://yanliu1111.github.io/myBlogNextjsTs2.png"
+      />
+      <meta
+        property="og:description"
+        content="View my code blog - I would like to share my learning code journey with you. Follow me as I explore the world of software development and share my insights, tips, and tutorials. From beginner to advanced, there's something for everyone. Let's learn and grow together."
+      />
+      <meta property="og:site_name" content="Yan's Code Blog" />
+
       <link rel="icon" href="/web.ico"></link>
     </>
   );
