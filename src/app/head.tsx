5e503d2485eea9c64e31cf7c98be385e2d5eb246
yanliu1111 <liuyan0504@gmail.com>
2023-03-04 21:26:15 -0700

    steps: app/page done, posts/page setting done

---
+++ src/app/head.tsx
@@ -0,0 +1,9 @@
+export default function Head() {
+  return (
+    <>
+      <title>Yan's Code Blog</title>
+      <meta content="width= device-width, initial-scale=1" name="description" />
+      <link rel="icon" href="/favicon.ico"></link>
+    </>
+  );
+}
---
+++ src/app/page.tsx
@@ -0,0 +1,23 @@
+import * as fs from "fs";
+import Link from "next/link";
+
+const getPostMetadata = () => {
+  const folder = "posts/";
+  const files = fs.readdirSync(folder);
+  const markdownPosts = files.filter((file) => file.endsWith(".md"));
+  const slugs = markdownPosts.map((file) => file.replace(".md", ""));
+  return slugs;
+};
+
+const HomePage = () => {
+  const postMetadata = getPostMetadata();
+  const postPreviews = postMetadata.map((slug, index) => (
+    <div key={index}>
+      <Link href={`/posts/${slug}`}>
+        <h2>{slug}</h2>
+      </Link>
+    </div>
+  ));
+  return <div>{postPreviews}</div>;
+};
+export default HomePage;
